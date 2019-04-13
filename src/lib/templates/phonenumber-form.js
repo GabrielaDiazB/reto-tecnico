030598ea9b4201a3017f591792da69a0ea773e38
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-04-13 07:52:59 -0500

    Se corrigieron errores de css y de identado

--- src/lib/templates/phonenumber-form.js
+++ src/lib/templates/phonenumber-form.js
@@ -5,7 +5,7 @@ export const secondForm = () => {
     <form class="secondpage-form">
         <label class="input-label phone-label"><img src="./images/US-Flag-icon.png" alt="usa-flag" class="flag-number">+1</label>
         <input type="tel" name="phone" placeholder="Phone Number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" class="phone-input font-input" required>
-        <button type="button" class="next-btn">NEXT</button>
+        <button type="button" class="next-btn">VERIFY</button>
         <hr>
         <p class="description">We collect your phone number to verify your identity and deliver a notification.</p>
     </form>
