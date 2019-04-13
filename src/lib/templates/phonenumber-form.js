c03278620662275b488e09a35e9c8498e0a8cdae
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-04-13 08:06:34 -0500

    correcciones de interfaz

--- src/lib/templates/phonenumber-form.js
+++ src/lib/templates/phonenumber-form.js
@@ -3,7 +3,7 @@ export const secondForm = () => {
     <div class="container second-form-box">
     <p class ="instructions">What is your cel phone number? We'll send you a one-time True Fan Verification Number.</p>
     <form class="secondpage-form">
-        <label class="input-label phone-label"><img src="./images/US-Flag-icon.png" alt="usa-flag" class="flag-number">+1</label>
+        <label class="input-label phone-label"><img src="./images/US-Flag-icon.png" alt="usa-flag" class="flag-number"> +1</label>
         <input type="tel" name="phone" placeholder="Phone Number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" class="phone-input font-input" required>
         <button type="button" class="next-btn">VERIFY</button>
         <hr>
